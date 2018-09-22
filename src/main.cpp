e4ce84e8a87765f7e204b78312d08e4f42ea6302
jeron7 <jeronimo.araujo@ccc.ufcg.edu.br>
2018-09-22 16:44:20 -0300

    Spawn de plataformas adicionado

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <termios.h>
 #include <ncurses.h> // Biblioteca responsável pelo mvprintw
@@ -8,12 +9,14 @@
 #include <ctype.h>
 
 #define VERTICAL 36
-#define HORIZONTAL 50
+#define HORIZONTAL 70
 #define DOWN 115
 #define UP 119
 #define LEFT 97
 #define RIGHT 100
 #define GRAVITY 3
+
+int score = 0;
 
 typedef struct Player {
 	int x, y; // Posição na tela
@@ -25,39 +28,43 @@ typedef struct Player {
 } Player;
 
 typedef struct Platform {
-    int X;
-    int Y;
-    char symbol;
-
+    int x, y;
+	int length;
+	int CONSTRUCTION_SPEED;
+    char symbol[2];
 } Platform;
 
 //Game
-void GameInit(Player *player);
-void setBorders();
+void GameInit(Player *player, Platform *latform);
+void PlatformInit(Platform *platform);
+void spawnPlatform(Platform *platform);
+void drawPlatform(Platform *platform, int sideSpawn, int direction);
 // Player
 void PlayerInit(Player *player);
-void PlatformInit(Platform *platform);
 void drawPlayer(Player *player);
 void erasePlayer(Player *player);
 void movePlayer(Player *player);
 void playerJump(Player *);
 // Util
-void debug(const char * msg);
+// void debug(const char * msg);
+void setBorders();
 
 int main() {
-	char  cmd;
 	initscr();
 	keypad(stdscr, TRUE);
 	noecho();
  	curs_set(FALSE); //Deixa o cursor invisível
 
 	Player player;
-	GameInit(&player);
-
-	int z = 0;
+	Platform platform;
+	GameInit(&player, &platform);
+	char cmd;
 	while(1) {
 		cmd = toupper(getchar());
 		switch (cmd) {
+			case 'P':
+				spawnPlatform(&platform);
+				break;
 			case 'W':
 			case UP:
 				movePlayer(&player);
@@ -70,11 +77,21 @@ int main() {
 	return 0;
 }
 
-void GameInit (Player *player) {
+void GameInit (Player *player, Platform *platform) {
 	PlayerInit(player);
+	PlatformInit(platform);
 	setBorders();
 	drawPlayer(player);
-} 
+	refresh();
+}
+
+void PlatformInit(Platform *platform) {
+	platform->x = HORIZONTAL;
+	platform->y = VERTICAL;
+	platform->length = (int) (HORIZONTAL/3) + 20;
+	strcpy(platform->symbol, "=");
+	platform->CONSTRUCTION_SPEED = 30000;
+}
 
 void PlayerInit(Player *player) {
 	player->height = 3;
@@ -82,6 +99,25 @@ void PlayerInit(Player *player) {
 	player->x = (int) (HORIZONTAL/2) - 2;
 	player->y = (int) VERTICAL - 3;
 	player->MAX_SPEED = 85500;	
+}
+
+void spawnPlatform(Platform *platform) {
+	int initSpawn = rand() % 18;
+	if (rand() % 2 == 1)
+		drawPlatform(platform, HORIZONTAL - initSpawn, -1);
+	else
+		drawPlatform(platform, initSpawn, 1);	
+}
+
+void drawPlatform(Platform *platform, int sideSpawn, int direction) {
+	int j;
+	for (j = 1; j <= platform->length + (rand () % 5); j++) {
+		int y = sideSpawn + (j * direction );
+		mvprintw(VERTICAL - score - 1, y, platform->symbol);
+		refresh();
+		usleep(platform->CONSTRUCTION_SPEED);
+	}	
+	score++;
 }
 
 void drawPlayer(Player *player) {
@@ -98,45 +134,38 @@ void erasePlayer(Player *player) {
 }
 
 void movePlayer(Player *player) {
-	int i = 0;
-
+	int i;
     for(i = 1; i <= GRAVITY; i++) {
         erasePlayer(player);
 		player->y -= i;
 		drawPlayer(player);
         usleep(player->MAX_SPEED);
 	}
-	for(i = 0; i <= GRAVITY; i++) {
+	for(i = 1; i <= GRAVITY; i++) {
 		erasePlayer(player);
 		player->y += i;
 		drawPlayer(player);
-         usleep(player->MAX_SPEED);
+        usleep(player->MAX_SPEED);
     }
 }
 
 void setBorders() {
-	
-    // int i;
-	//for(i = 0; i < 20; i++) {
-		for (int j = 0; j < HORIZONTAL + 1; j++) {
-			if(j + 12 <= HORIZONTAL)
-				mvprintw(0, j + 12, "X");
-			mvprintw(VERTICAL, j, "X");
-		}
-		mvprintw(0, 1, " Pontuação:        ");
-	//}
-	//for(i = 0; i < 20; i++) {
-		for (int j = 0; j < VERTICAL; j++) {
-			mvprintw(j, 0, "&");
-			mvprintw(j, HORIZONTAL, "&");
-   		}
-	//}
-
+	for (int j = 0; j < HORIZONTAL + 1; j++) {
+		if(j + 12 <= HORIZONTAL)
+			mvprintw(0, j + 14, "X");
+		mvprintw(VERTICAL, j, "X");
+	}
+	mvprintw(0, 1, " Pontuação: %06d ", score);
+	for (int j = 0; j < VERTICAL; j++) {
+		mvprintw(j, 0, "X");
+		mvprintw(j, HORIZONTAL, "X");
+	}
 }
 
-void debug(const char * msg) {
-	fprintf(stderr, msg);
-}
+// void debug(const char * msg) {
+// 	fprintf(stderr, msg);
+// }
+
 
 // int kbhit(void) {
 //   struct termios oldt, newt;
