8ee98ebaf90b388da44552da87457c190958dae7
jeron7 <jeronimo.araujo@ccc.ufcg.edu.br>
2018-09-23 13:12:06 -0300

    usleep no loop principal

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,17 @@
 #include <fcntl.h>
 #include <cstdlib>
 #include <ctype.h>
+#include <time.h>
 
-#define VERTICAL 36
-#define HORIZONTAL 70
+#define VERTICAL 37
+#define HORIZONTAL 80
 #define DOWN 115
 #define UP 119
 #define LEFT 97
 #define RIGHT 100
 #define GRAVITY 3
 
-int score = 0;
+int score = -1;
 
 typedef struct Player {
 	int x, y; // Posição na tela
@@ -28,17 +29,21 @@ typedef struct Player {
 } Player;
 
 typedef struct Platform {
-    int x, y;
-	int length;
-	int CONSTRUCTION_SPEED;
+    int endX, startX, y;
     char symbol[2];
+	int direction;
 } Platform;
+
+int kbhit(void);
 
 //Game
 void GameInit(Player *player, Platform *latform);
+//Platform
 void PlatformInit(Platform *platform);
 void spawnPlatform(Platform *platform);
-void drawPlatform(Platform *platform, int sideSpawn, int direction);
+void drawHeadPlatform(Platform *platform);
+void movePlatformHead(Platform *platform);
+int finishedConstruction(Platform *platform);
 // Player
 void PlayerInit(Player *player);
 void drawPlayer(Player *player);
@@ -46,7 +51,7 @@ void erasePlayer(Player *player);
 void movePlayer(Player *player);
 void playerJump(Player *);
 // Util
-// void debug(const char * msg);
+void debug(const char * msg);
 void setBorders();
 
 int main() {
@@ -58,20 +63,39 @@ int main() {
 	Player player;
 	Platform platform;
 	GameInit(&player, &platform);
-	char cmd;
-	while(1) {
-		cmd = toupper(getchar());
+
+	while (1) {
+		// Enquanto teclado não utilizado, fica neste loop
+		while (!kbhit()) {
+			erasePlayer(&player);
+			movePlayer(&player);
+			drawPlayer(&player);
+			if (finishedConstruction(&platform))
+				spawnPlatform(&platform);
+			drawHeadPlatform(&platform);
+			movePlatformHead(&platform);
+			setBorders();
+			usleep(player.MAX_SPEED);
+			refresh();
+			//if (gameOver(&player)) {
+			//	 break;
+			//}
+		}
+
+		char cmd = toupper(getchar());
 		switch (cmd) {
 			case 'P':
 				spawnPlatform(&platform);
 				break;
 			case 'W':
-			case UP:
-				movePlayer(&player);
+				playerJump(&player);
 				break;
 			case 'Q':
 				endwin();
 				exit(0);
+			default:
+				printf("\a");
+				break;
 		}
 	}
 	return 0;
@@ -82,15 +106,14 @@ void GameInit (Player *player, Platform *platform) {
 	PlatformInit(platform);
 	setBorders();
 	drawPlayer(player);
-	refresh();
 }
 
 void PlatformInit(Platform *platform) {
-	platform->x = HORIZONTAL;
-	platform->y = VERTICAL;
-	platform->length = (int) (HORIZONTAL/3) + 20;
+	platform->endX = 0;
+	platform->startX = 0;
+	platform->y = VERTICAL - 3;
+	platform->direction = 0;
 	strcpy(platform->symbol, "=");
-	platform->CONSTRUCTION_SPEED = 30000;
 }
 
 void PlayerInit(Player *player) {
@@ -98,33 +121,17 @@ void PlayerInit(Player *player) {
 	player->width = 3;
 	player->x = (int) (HORIZONTAL/2) - 2;
 	player->y = (int) VERTICAL - 3;
-	player->MAX_SPEED = 85500;	
+	player->MAX_SPEED = 100000;	
 }
 
-void spawnPlatform(Platform *platform) {
-	int initSpawn = rand() % 18;
-	if (rand() % 2 == 1)
-		drawPlatform(platform, HORIZONTAL - initSpawn, -1);
-	else
-		drawPlatform(platform, initSpawn, 1);	
-}
-
-void drawPlatform(Platform *platform, int sideSpawn, int direction) {
-	int j;
-	for (j = 1; j <= platform->length + (rand () % 5); j++) {
-		int y = sideSpawn + (j * direction );
-		mvprintw(VERTICAL - score - 1, y, platform->symbol);
-		refresh();
-		usleep(platform->CONSTRUCTION_SPEED);
-	}	
-	score++;
+void playerJump(Player *player) {
+	player->yVelocity = -6;
 }
 
 void drawPlayer(Player *player) {
 	mvprintw(player->y, player->x, "  o  ");
 	mvprintw(1 + player->y, player->x, "/|_|\\");
 	mvprintw(2 + player->y, player->x, " / \\ ");
-	refresh();
 }
 
 void erasePlayer(Player *player) {
@@ -134,59 +141,84 @@ void erasePlayer(Player *player) {
 }
 
 void movePlayer(Player *player) {
-	int i;
-    for(i = 1; i <= GRAVITY; i++) {
-        erasePlayer(player);
-		player->y -= i;
-		drawPlayer(player);
-        usleep(player->MAX_SPEED);
+	if(player->y >= VERTICAL-3 && player->yVelocity >= 0) {
+		player->yVelocity = 0;
+	} else {
+		player->yVelocity += GRAVITY;
+		player->y += player->yVelocity;
 	}
-	for(i = 1; i <= GRAVITY; i++) {
-		erasePlayer(player);
-		player->y += i;
-		drawPlayer(player);
-        usleep(player->MAX_SPEED);
-    }
+	if (player->y > VERTICAL-3) player->y = VERTICAL - 3;
 }
 
 void setBorders() {
 	for (int j = 0; j < HORIZONTAL + 1; j++) {
-		if(j + 12 <= HORIZONTAL)
-			mvprintw(0, j + 14, "X");
+		if(j + 18 <= HORIZONTAL)
+			mvprintw(0, j + 20, "X");
 		mvprintw(VERTICAL, j, "X");
 	}
-	mvprintw(0, 1, " Pontuação: %06d ", score);
-	for (int j = 0; j < VERTICAL; j++) {
+	mvprintw(0, 1, " Pontuação: %03d ", score);
+	for (int j = 0; j <= VERTICAL; j++) {
 		mvprintw(j, 0, "X");
 		mvprintw(j, HORIZONTAL, "X");
 	}
 }
 
-// void debug(const char * msg) {
+void spawnPlatform(Platform *platform) {
+	srand(time(NULL));
+	int initSpawn = rand() % 18;
+	if (rand() % 2 == 0) {
+		platform->endX = initSpawn + (HORIZONTAL/2) + 10;
+		platform->startX = initSpawn;
+		platform->direction = -1;
+	} else {
+		platform->endX = (HORIZONTAL - initSpawn) - (HORIZONTAL/2) - 10;
+		platform->startX = (HORIZONTAL - initSpawn);
+		platform->direction = 1;
+	}	
+	platform->y = VERTICAL - score - 1;
+}
+
+void movePlatformHead(Platform *platform) {
+	if (platform->endX != platform->startX)
+		platform->startX -= platform->direction;
+}
+
+void drawHeadPlatform(Platform *platform) {
+	mvprintw(platform->y, platform->startX, platform->symbol);
+}
+
+int finishedConstruction(Platform *platform) {
+	if (platform->endX == platform->startX) {
+		score+= 1;
+		return 1;
+	}
+	return 0;
+}
+
+// void debug(const char *msg) {
 // 	fprintf(stderr, msg);
 // }
 
+int kbhit(void) {
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
 
-// int kbhit(void) {
-//   struct termios oldt, newt;
-//   int ch;
-//   int oldf;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
 
-//   tcgetattr(STDIN_FILENO, &oldt);
-//   newt = oldt;
-//   newt.c_lflag &= ~(ICANON | ECHO);
-//   tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-//   oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-//   fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+  ch = getchar();
 
-//   ch = getchar();
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
 
-//   tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-//   fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-//   if(ch != EOF) {
-//     ungetc(ch, stdin);
-//     return 1;
-//   }
-//   return 0;
-// }
+  if(ch != EOF) {
+    ungetc(ch, stdin);
+    return 1;
+  }
+  return 0;
+}
